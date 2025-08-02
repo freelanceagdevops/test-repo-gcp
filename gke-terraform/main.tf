0aadb255977c35ae6dee019f5da59cbfa2034c50
freelanceagdevops <freelanceagdevops@gmail.com>
2025-08-03 00:29:38 +0530

    Update main.tf

--- gke-terraform/main.tf
+++ gke-terraform/main.tf
@@ -1,17 +1,24 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+# Custom VPC Network
 resource "google_compute_network" "custom_network" {
   name                    = "custom-vpc"
   auto_create_subnetworks = false
 }
 
-resource "google_compute_subnetwork" "custom-subnet" {
+# Custom Subnetwork
+resource "google_compute_subnetwork" "custom_subnet" {
   name          = "custom-subnetwork"
   ip_cidr_range = "10.10.0.0/16"
   region        = var.region
   network       = google_compute_network.custom_network.id
 }
 
-# Firewall-1 for Internal Communication
-resource "google_compute_firewall" "allow-internal" {
+# Firewall Rule: Internal Communication
+resource "google_compute_firewall" "allow_internal" {
   name    = "internal-firewall"
   network = google_compute_network.custom_network.id
 
@@ -22,8 +29,8 @@ resource "google_compute_firewall" "allow-internal" {
   source_ranges = ["10.10.0.0/16"]
 }
 
-# Firewall-2 for External Access SSH, icmp, RDP
-resource "google_compute_firewall" "allow-external" {
+# Firewall Rule: External Access (SSH, ICMP, RDP)
+resource "google_compute_firewall" "allow_external" {
   name    = "external-firewall"
   network = google_compute_network.custom_network.id
 
@@ -39,8 +46,8 @@ resource "google_compute_firewall" "allow-external" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-# Firewall-3 for GKE Communication
-resource "google_compute_firewall" "allow-gke" {
+# Firewall Rule: GKE Communication
+resource "google_compute_firewall" "allow_gke" {
   name    = "gke-firewall"
   network = google_compute_network.custom_network.id
 
@@ -52,29 +59,31 @@ resource "google_compute_firewall" "allow-gke" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-
-# GKE Cluster
+# GKE Cluster (Testing)
 resource "google_container_cluster" "primary" {
   project             = var.project
   name                = "terraform-gke-cluster"
   location            = var.region
   network             = google_compute_network.custom_network.id
-  subnetwork          = google_compute_subnetwork.custom-subnet.id
+  subnetwork          = google_compute_subnetwork.custom_subnet.id
   deletion_protection = false
+
   remove_default_node_pool = true
-  initial_node_count       = 1  
+  initial_node_count       = 1
 }
 
+# GKE Node Pool (minimal setup for testing)
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name           = "my-node-pool"
-  cluster        = google_container_cluster.primary.name
-  location       = google_container_cluster.primary.location
-  node_count     = 1  
+  name       = "my-node-pool"
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  node_count = 1
 
   node_config {
-    machine_type   = "e2-medium"  # Use a smaller instance to reduce storage requirements
-    disk_size_gb   = 15
-    disk_type      = "pd-standard"
-    image_type     = "UBUNTU_CONTAINERD"
+    machine_type = "e2-medium"     # Budget-friendly VM type
+    disk_size_gb = 15
+    disk_type    = "pd-standard"
+    image_type   = "UBUNTU_CONTAINERD"
+    preemptible  = true            # Reduce cost for testing
   }
 }
