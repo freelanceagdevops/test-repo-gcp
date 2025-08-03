cba3785e7b0a39284e608bfdfc2989133da780d1
freelanceagdevops <freelanceagdevops@gmail.com>
2025-08-03 11:57:59 +0530

    Update main.tf

--- gke-terraform/main.tf
+++ gke-terraform/main.tf
@@ -6,11 +6,10 @@ resource "google_compute_network" "custom_network" {
 resource "google_compute_subnetwork" "custom-subnet" {
   name          = "custom-subnetwork"
   ip_cidr_range = "10.10.0.0/16"
-  zone          = "us-central1-a"
+  region        = var.region
   network       = google_compute_network.custom_network.id
 }
 
-# Firewall-1 for Internal Communication
 resource "google_compute_firewall" "allow-internal" {
   name    = "internal-firewall"
   network = google_compute_network.custom_network.id
@@ -22,7 +21,6 @@ resource "google_compute_firewall" "allow-internal" {
   source_ranges = ["10.10.0.0/16"]
 }
 
-# Firewall-2 for External Access SSH, icmp, RDP
 resource "google_compute_firewall" "allow-external" {
   name    = "external-firewall"
   network = google_compute_network.custom_network.id
@@ -39,7 +37,6 @@ resource "google_compute_firewall" "allow-external" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-# Firewall-3 for GKE Communication
 resource "google_compute_firewall" "allow-gke" {
   name    = "gke-firewall"
   network = google_compute_network.custom_network.id
@@ -52,29 +49,27 @@ resource "google_compute_firewall" "allow-gke" {
   source_ranges = ["0.0.0.0/0"]
 }
 
-
-# GKE Cluster
 resource "google_container_cluster" "primary" {
   project             = var.project
   name                = "terraform-gke-cluster"
-  zone                = "us-central1-a"
+  zone                = var.zone            # ✅ Zonal Cluster
   network             = google_compute_network.custom_network.id
   subnetwork          = google_compute_subnetwork.custom-subnet.id
   deletion_protection = false
   remove_default_node_pool = true
-  initial_node_count       = 1  
+  initial_node_count       = 1
 }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
-  name           = "my-node-pool"
-  cluster        = google_container_cluster.primary.name
-  location       = google_container_cluster.primary.location
-  node_count     = 1  
+  name       = "my-node-pool"
+  cluster    = google_container_cluster.primary.name
+  zone       = var.zone                     # ✅ Explicitly define zone
+  node_count = 1
 
   node_config {
-    machine_type   = "e2-medium"  # Use a smaller instance to reduce storage requirements
-    disk_size_gb   = 15
-    disk_type      = "pd-standard"
-    image_type     = "UBUNTU_CONTAINERD"
+    machine_type = "e2-medium"
+    disk_size_gb = 15
+    disk_type    = "pd-standard"
+    image_type   = "UBUNTU_CONTAINERD"
   }
 }
